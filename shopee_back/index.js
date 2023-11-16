b23af4533380e85e9c503af896e85e298abd400f
Cuong <nguyenvietcuong2901@gmail.com>
2023-11-16 20:45:46 +0700

    update index.js file, fix bug username existed

--- shopee_back/index.js
+++ shopee_back/index.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
 const cors = require("cors");
-
+// hello bà con
 const app = express();
 const port = 3000;
 app.use(cors());
@@ -30,17 +30,20 @@ app.use(bodyParser.json());
 
 app.post("/register", async (req, res) => {
   try {
-    //const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    // const token = jwt.sign({ email: req.body.email }, "secret", {
-    //   expiresIn: "1d",
-    // }); // Tạo token xác minh email
-    const newUser = new User({
-      email: req.body.email,
-      password: req.body.password, //hashedPassword,
-    });
-    await newUser.save();
-    //sendVerificationEmail(req.body.email, token); // Gửi email xác minh
-    res.send("Đăng ký thành công!");
+    const userFound = await User.findOne({ email: req.body.email });
+
+    if (userFound) {
+      res.status(500).send("email da ton tai!");
+    } else {
+      const newUser = new User({
+        email: req.body.email,
+        password: req.body.password, //hashedPassword,
+      });
+
+      await newUser.save();
+
+      res.send("Đăng ký thành công!");
+    }
   } catch {
     res.status(500).send("Lỗi khi đăng ký người dùng!");
   }
@@ -52,7 +55,6 @@ app.post("/login", async (req, res) => {
 
   const user = await User.findOne({ email: email });
   if (user) {
-    //const match = password === user.password;
     if (password === user.password) {
       res.send("Đăng nhập thành công!");
     } else {
